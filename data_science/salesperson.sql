06a1fb0c9189d2d53dd961f15e81c76af4edd159
kfpotts1 <kfpotts1@gmail.com>
2017-04-21 13:17:21 -0400

    added to data_science

--- data_science/salesperson.sql
+++ data_science/salesperson.sql
@@ -12,20 +12,20 @@ CREATE TABLE Orders (
     Amount INTEGER);
     
     
-INSERT INTO Salesperson(ID, Name, Age, Salary) VALUES (1, "Abe", 61, 140000);
-INSERT INTO Salesperson(ID, Name, Age, Salary) VALUES (2, "Bob", 34, 44000);
-INSERT INTO Salesperson(ID, Name, Age, Salary) VALUES (5, "Chris", 34, 40000);
-INSERT INTO Salesperson(ID, Name, Age, Salary) VALUES (7, "Dan", 41, 52000);
-INSERT INTO Salesperson(ID, Name, Age, Salary) VALUES (8, "Ken", 57, 115000);
-INSERT INTO Salesperson(ID, Name, Age, Salary) VALUES (11, "Joe", 38, 38000);
+INSERT INTO Salesperson(ID, Name, Age, Salary) VALUES (1, "Kyle", 61, 165000);
+INSERT INTO Salesperson(ID, Name, Age, Salary) VALUES (2, "Kelsey", 34, 34000);
+INSERT INTO Salesperson(ID, Name, Age, Salary) VALUES (5, "Cam", 34, 105000);
+INSERT INTO Salesperson(ID, Name, Age, Salary) VALUES (7, "Paul", 41, 31000);
+INSERT INTO Salesperson(ID, Name, Age, Salary) VALUES (8, "Charlie", 57, 63000);
+INSERT INTO Salesperson(ID, Name, Age, Salary) VALUES (11, "Syd", 38, 92000);
 
-INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES(10, "8/2/96", 4, 2, 540);
-INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES (20, "1/30/99", 4, 8, 1800);
-INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES (30, "7/14/95", 9, 1, 460);
-INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES (40, "1/29/98", 7, 2, 2400);
-INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES (50, "2/3/98", 6, 7, 600);
-INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES (60, "3/2/98", 6, 7, 720);
-INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES (70, "5/6/98", 9, 7, 150);
+INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES(10, "1/2/94", 4, 2, 800);
+INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES (20, "1/30/98", 4, 8, 510);
+INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES (30, "7/14/91", 9, 1, 2160);
+INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES (40, "1/29/95", 7, 2, 600);
+INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES (50, "2/2/97", 6, 7, 820);
+INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES (60, "1/3/97", 6, 7, 520);
+INSERT INTO Orders(Number, order_date, cust_id, salesperson_id, Amount) VALUES (70, "8/1/99", 9, 7, 350);
 
 -- names of salespersons with more than 1 sale
 SELECT Salesperson.name FROM Salesperson JOIN Orders ON Orders.salesperson_id = Salesperson.ID GROUP BY Salesperson.ID HAVING COUNT(*) > 1;
